966d7adb222db5d4a05ab47b29dd817ad6109a64
Mokwuah <chidieberemokwuah@gmail.com>
2025-09-18 14:06:27 +0100

    new project

--- tictactoe.js
+++ tictactoe.js
@@ -11,7 +11,7 @@ function makeMove(turn, board){
         console.log("Invalid Row.")
     }else if(isNaN(col) || col < 1 || col > 3){
         console.log("Invalid Column.")
-    }else if(board[row -1][col - 1]!= " "){
+    }else if(board[row - 1][col - 1]!= " "){
         console.log("Invalid Position.")
     }
     else{
@@ -20,10 +20,71 @@ function makeMove(turn, board){
     }
   }  
 }
+function printBoard(board){
+    for (let i = 0; i < board.length; i++){
+        const row = board[i]
+        let rowString = ""
+        for(let j = 0; j < row.length; j++){
+            rowString += row[j]
+            if(j != row.length -1 ) rowString += " | "
+        }
+        console.log(rowString)
+        if(i !== board.length - 1) console.log("----------")
+ }
+}
+   
+
+function winCon(board, turn){
+    const lines = [
+        [[0,0], [0,1], [0,2]], //row0
+        [[1,0], [1,1], [1,2]], //row1
+        [[2,0], [2,1], [2,2]], //row2
+        [[0,0], [1,0], [2,0]], //col0
+        [[0,1], [1,1], [2,1]], //col1
+        [[0,2], [1,2], [2,2]], //col2
+        [[0,0], [1,1], [2,2]], //diagonal1
+        [[0,2], [1,1], [2,0]]  //diagonal2
+    ]
+    for(let line of lines){
+        let win = true
+        for(let pos of line){
+            const [row, col] = pos
+            if(board[row][col] != turn) {
+                win = false; 
+                break;
+            } 
+        }
+        if(win) return true;
+    }
+    return false;
+}
 
 const board = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
 ]
+
+let turn = "X";
+let turnCount = 0;
+
+
+printBoard(board)
+console.log()
+while(turnCount < 9){
+    console.log("It is the ", turn, "Players Turn")
+    makeMove(turn, board)
+    printBoard(board)
+    console.log()
+    const win = winCon(board, turn)
+    if (win){
+        console.log(turn, "has won")
+        break;
+    }
+
+    if(turn === "X") turn = "O"
+    else turn = "X"
+     turnCount++;
+
+    } 
 
